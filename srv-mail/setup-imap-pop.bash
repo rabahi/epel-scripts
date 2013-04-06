9adb0254d3b7efa7a0bb0aa9f007b6b838e3d29d
cousiano <marc.rabahi@gmail.com>
2013-04-06 18:55:24 +0200

    firewall rules.

--- srv-mail/setup-imap-pop.bash
+++ srv-mail/setup-imap-pop.bash
@@ -10,7 +10,8 @@ echo "start service"
 service dovecot start
 
 
-echo "open smtp port (i.e 25)"
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 25 -j ACCEPT
+echo "open pop3 port (i.e 110) and imap (i.e. 143)"
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 110 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 143 -j ACCEPT
 service iptables save
 service iptables restart

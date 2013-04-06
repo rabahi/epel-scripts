da0c37b7858364244057f4accd397cf1d2c1325a
cousiano <marc.rabahi@gmail.com>
2013-04-06 17:25:06 +0200

    configuration of nagios nrpe.

--- prerequistes/nagios-nrpe.bash
+++ prerequistes/nagios-nrpe.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "install nrpe"
-yum -y install nrpe
+yum -y install nrpe nagios-plugins*
 
 echo "start nrpe on boot"
 chkconfig nrpe on
@@ -11,5 +11,13 @@ iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 5666 -j ACCEPT
 service iptables save
 service iptables restart
 
+echo "sudoers configuration (deal with issue 'NRPE: Unable to read output')"
+chmod 755 /etc/sudoers
+sed -i "s/^\(Defaults\s*requiretty\)/#\1/" /etc/sudoers
+chmod 0440 /etc/sudoers
+
+echo "allow every one to connect to nrpe"
+sed -i "s/^\(allowed_hosts\)/#\1/" /etc/nagios/nrpe.cfg
+  
 echo "start nrpe"
 service nrpe start

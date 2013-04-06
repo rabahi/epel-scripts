6db325f53b2bdcd08e18b3119f8473bcf82d2e53
cousiano <marc.rabahi@gmail.com>
2013-04-06 19:41:46 +0200

    check_http get a 403 'forbidden' if index.html is missing. so we create it

--- srv-monitoring/nagios.bash
+++ srv-monitoring/nagios.bash
@@ -6,6 +6,9 @@ yum -y install nagios nagios-devel nagios-plugins*
 echo "enable start nagios on boot"
 chkconfig nagios on
  
+echo "check_http get a 403 'forbidden' if index.html is missing. so we create it"
+touch /var/www/html/index.html
+ 
 myip=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 echo "Now meet you here: http://$myip/nagios/"
 echo "Note, login/passord is nagiosadmin/nagiosadmin"

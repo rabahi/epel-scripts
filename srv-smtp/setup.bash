f6b726c4e5243a1330caebf55d4e74b9245a3bfb
cousiano <marc.rabahi@gmail.com>
2013-04-06 15:17:51 +0200

    open smtp port (i.e 25)

--- srv-smtp/setup.bash
+++ srv-smtp/setup.bash
@@ -19,3 +19,8 @@ sed -i "s/^#\(mydestination = \$myhostname, localhost.\$mydomain, localhost, \$m
 
 echo "reload postfix"
 service postfix reload
+
+echo "open smtp port (i.e 25)"
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 25 -j ACCEPT
+service iptables save
+service iptables restart

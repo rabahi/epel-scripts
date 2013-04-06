9e8a549e11944168d7f0dbece3f56d10d95907d1
cousiano <marc.rabahi@gmail.com>
2013-04-06 22:51:29 +0200

    add parameters.

--- srv-intranet/wordpress.bash
+++ srv-intranet/wordpress.bash
@@ -1,21 +1,34 @@
 #!/bin/bash
 
+##################################################
+#      PARAMETERS 
+##################################################
+
+wp_database_name=wordpress
+wp_username=wordpress
+wp_password=wordpress
+
+
+##################################################
+#      INSTALLATION SCRIPT
+##################################################
+
 echo "install wordpress"
 yum -y install wordpress
  
-echo "create database wordpress, user/password wordpress/wordpress":
-mysql --user=root --password=root -e "CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'wordpress';"
-mysql --user=root --password=root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
-mysql --user=root --password=root -e "use wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' WITH GRANT OPTION;"
+echo "create database $wp_database_name, user/password $wp_username/$wp_password":
+mysql --user=root --password=root -e "CREATE USER '$wp_username'@'localhost' IDENTIFIED BY '$wp_password';"
+mysql --user=root --password=root -e "CREATE DATABASE IF NOT EXISTS $wp_database_name;"
+mysql --user=root --password=root -e "use $wp_database_name; GRANT ALL PRIVILEGES ON $wp_database_name.* TO '$wp_username'@'localhost' WITH GRANT OPTION;"
 
 echo "Note: by default only local users can access to wordpress."
 echo "Let's update the file /etc/httpd/conf.d/wordpress.conf and allow everyone."
 sed -i "s/\(Deny\s*from\s*All\)/#\1/" /etc/httpd/conf.d/wordpress.conf
 
 echo "configure the database connection here: /usr/share/wordpress/wp-config.php"
-sed -i "s/\database_name_here/wordpress/" /usr/share/wordpress/wp-config.php
-sed -i "s/\username_here/wordpress/" /usr/share/wordpress/wp-config.php
-sed -i "s/\password_here/wordpress/" /usr/share/wordpress/wp-config.php
+sed -i "s/\database_name_here/$wp_database_name/" /usr/share/wordpress/wp-config.php
+sed -i "s/\username_here/$wp_username/" /usr/share/wordpress/wp-config.php
+sed -i "s/\password_here/$wp_password/" /usr/share/wordpress/wp-config.php
 
 echo "restart httpd"
 service httpd restart

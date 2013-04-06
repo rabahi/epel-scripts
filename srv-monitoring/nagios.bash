fbd94b1eacadc500df85346f578555c7655a2eb0
cousiano <marc.rabahi@gmail.com>
2013-04-06 11:19:48 +0200

    enable start nagios on boot

--- srv-monitoring/nagios.bash
+++ srv-monitoring/nagios.bash
@@ -5,7 +5,9 @@ rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.r
  
 echo "install tools nagios"
 yum -y install nagios nagios-devel nagios-plugins*
- 
+
+echo "enable start nagios on boot"
+chkconfig nagios on
  
 myip=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 echo "Now meet you here: http://$myip/nagios/"

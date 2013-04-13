7d215f96577f6dc4c83fb124397cade1925ea74d
cousiano <marc.rabahi@gmail.com>
2013-04-13 15:57:06 +0200

    add udp ports for versions 2,3,4.

--- Other/srv-nfs/setup-nfs.bash
+++ Other/srv-nfs/setup-nfs.bash
@@ -10,8 +10,9 @@ service rpcbind start
 service nfs start
  
 echo "add right to anonymous user NFS (id 65534)"
+mkdir -p /opt/nfs
 chown 65534:65534 /opt/nfs
-chmod 755 /opt/nfs 
+chmod 755 /opt/nfs
  
 echo "add rule to enable connection for client 192.168.0.10 (must set ip address)"
 cat >> /etc/exports << "EOF"
@@ -20,3 +21,10 @@ EOF
  
 echo "enable rules"
 exportfs -a
+
+echo "Open ports 111,2049,4045"
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 111 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2049 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 4045 -j ACCEPT
+service iptables save
+service iptables restart

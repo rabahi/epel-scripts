6d8e15ba158899c877c22ced38014bce197a59fe
cousiano <marc.rabahi@gmail.com>
2013-04-14 16:06:40 +0200

    configure srv-vpn

--- Network/srv-vpn/setup-vpn.bash
+++ Network/srv-vpn/setup-vpn.bash
@@ -15,5 +15,15 @@ echo "configure vpn"
 rm -f /etc/openvpn/server.conf
 cp /usr/share/doc/openvpn-*/sample-config-files/server.conf /etc/openvpn/ 
 
+sed -i "s/dev tun/dev tap0/g" /etc/openvpn/server.conf
+sed -i "s/^\(ca \)/#\1/g" /etc/openvpn/server.conf
+sed -i "s/^\(cert \)/#\1/g" /etc/openvpn/server.conf
+sed -i "s/^\(key \)/#\1/g" /etc/openvpn/server.conf
+sed -i "s/^\(dh \)/#\1/g" /etc/openvpn/server.conf
+sed -i "s/^\(server \)/#\1/g" /etc/openvpn/server.conf
+sed -i "s/^\(ifconfig-pool-persist \)/#\1/g" /etc/openvpn/server.conf
+sed -i "s/^\;\(log\)/\1/g" /etc/openvpn/server.conf
+sed -i "s/\(openvpn.log\)/\/var\/log\/\1/g" /etc/openvpn/server.conf
+
 echo "start service"
 service openvpn start

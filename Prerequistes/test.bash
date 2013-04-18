378f593a774718e7cb2f0aaace806d1eb5660afe
cousiano <marc.rabahi@gmail.com>
2013-04-18 14:35:16 +0200

    webmin visible on /webmin.

--- Prerequistes/test.bash
+++ Prerequistes/test.bash
@@ -31,5 +31,5 @@ check_tcp 5666 #nrpe
 check_tcp 10000 #webmin
 
 # check pages
-check_web_function Webmin http://localhost:10000/
+check_web_function Webmin http://localhost/webmin/
 check_web_function phpMyAdmin "http://localhost/phpMyAdmin/ --http-user=root --http-password=root"
--- Prerequistes/webmin.bash
+++ Prerequistes/webmin.bash
@@ -3,10 +3,22 @@
 echo "install webmin"
 yum -y install webmin
 
-echo "open port 10000"
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 10000 -j ACCEPT
-service iptables save
-service iptables restart
+echo "configure webmin"
+echo "webprefix=/webmin" >> /etc/webmin/config
+echo "webprefixnoredir=1" >> /etc/webmin/config
+echo "referers=localhost" >> /etc/webmin/config
+echo "referer=1" >> /etc/webmin/config
+
+cat > /etc/httpd/conf.d/webmin.conf << "EOF"
+ProxyPass /webmin/ http://localhost:10000/
+ProxyPassReverse /webmin/ http://localhost:10000/
+<Proxy *>
+allow from all
+</Proxy>
+EOF
+
+echo "reload httpd"
+service httpd reload
 
 echo "launch webmin"
 service webmin start

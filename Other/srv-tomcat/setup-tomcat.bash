c568cfa5ed4d54bad05a562075088593e09fd7c8
cousiano <marc.rabahi@gmail.com>
2014-08-01 12:26:51 +0200

    allow service to start with an other user (default root)

--- Other/srv-tomcat/setup-tomcat.bash
+++ Other/srv-tomcat/setup-tomcat.bash
@@ -9,55 +9,53 @@ wget -O /opt/java/apache-tomcat-6.0.37.tar.gz http://mirrors.ircam.fr/pub/apache
 cd /opt/java
 tar xvfz apache-tomcat-6.0.37.tar.gz
 
+ln -s /opt/java/apache-tomcat-6.0.37 /opt/java/apache-tomcat
+
 echo "create service /etc/init.d/tomcat6"
 cat > /etc/init.d/tomcat6 << "EOF"
 #!/bin/bash
 # chkconfig: 234 20 80
 # description: Tomcat Server basic start/shutdown script
 # processname: tomcat6
+# pidfile: /opt/java/apache-tomcat/temp/pid
 
-tomcat=/opt/java/apache-tomcat-6.0.37
-startup=$tomcat/bin/startup.sh
-shutdown=$tomcat/bin/shutdown.sh
+. /etc/rc.d/init.d/functions
 
-start() {
-  echo -n $"Starting Tomcat service: "
-  sh $startup
-  echo $?
-}
+# Set Tomcat environment.
+USER=root
+export PROCESSNAME=tomcat6
+export BASEDIR=/opt/java/apache-tomcat
+export CATALINA_HOME=$BASEDIR
+export CATALINA_BASE=$BASEDIR
+export CATALINA_PID=$BASEDIR/temp/pid
+export CATALINA_OPTS="-Xmx512m -Djava.awt.headless=true"
+LOCKFILE=$BASEDIR/temp/lockfile
 
-stop() {
-  echo -n $"Stopping Tomcat service: "
-  sh $shutdown
-  echo $?
-}
-
-restart() {
-  stop
-  start
-}
-
-status() {
-  ps -aef | grep apache-tomcat | grep -v tomcat6 | grep -v grep
-}
-
-# Handle the different input options
 case "$1" in
-start)
-  start
-  ;;
-stop)
-  stop
-  ;;
-status)
-  status
-  ;;
-restart)
-  restart
-  ;;
-*)
-  echo $"Usage: $0 {start|stop|restart|status}"
-  exit 1
+  start)
+        echo -n "Starting service $PROCESSNAME: "
+        status -p $CATALINA_PID $PROCESSNAME > /dev/null && failure || (su -p -s /bin/sh $USER -c "cd $CATALINA_HOME && $CATALINA_HOME/bin/catalina.sh start" > /dev/null && (touch $LOCKFILE ; success))
+        echo
+        ;;
+  stop)
+        echo -n "Shutting down service $PROCESSNAME: "
+        status -p $CATALINA_PID $PROCESSNAME > /dev/null && su -p -s /bin/sh $USER -c "cd $CATALINA_HOME && $CATALINA_HOME/bin/catalina.sh stop" > /dev/null && (rm -f $LOCKFILE ; success) || failure
+        echo
+        ;;
+  restart)
+        $0 stop
+        $0 start
+        ;;
+  condrestart)
+       [ -e $LOCKFILE ] && $0 restart
+       ;;
+  status)
+        status -p $CATALINA_PID $PROCESSNAME
+        ;;
+  *)
+        echo "Usage: $0 {start|stop|restart|condrestart|status}"
+        exit 1
+        ;;
 esac
 
 exit 0

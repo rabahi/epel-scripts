ff8fe4d8e8bb24065c9eb0c4b7945ef441ff4ca8
rabahi <marc.rabahi@gmail.com>
2016-10-08 13:54:30 +0200

    Update test.bash

--- Other/srv-nfs/test.bash
+++ Other/srv-nfs/test.bash
@@ -6,13 +6,14 @@
 # check services:
 check_service rpcbind
 check_service nfs
+check_service nfs-server
 
 # check udp port:
 check_udp 111  #portmap
 check_udp 2049 #nfsd
-#check_udp 4045 #NFS lock manager
+check_udp 4045 #NFS lock manager
 
 # check tcp port:
 check_tcp 111  #portmap
 check_tcp 2049 #nfsd
-#check_tcp 4045 #NFS lock manager
+check_tcp 4045 #NFS lock manager

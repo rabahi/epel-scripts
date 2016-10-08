d73b388895d58c03eedb524d8c7180f9f8ddaff5
rabahi <marc.rabahi@gmail.com>
2016-10-08 11:39:41 +0200

    Update network.bash

--- Prerequistes/network.bash
+++ Prerequistes/network.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "start manually"
-ifup ens33
+# get current device (i.e. ens33)
+currentDevice=`nmcli d | grep connected | awk '{split($1,a,"\t"); print a[1]}'`
 
-echo "edit /etc/sysconfig/network-scripts/ifcfg-ens33 and set ONBOOT=yes"
-sed -i "s/^\(ONBOOT=\).*$/\1yes/g" /etc/sysconfig/network-scripts/ifcfg-ens33
+echo "start manually device $currentDevice"
+ifup $currentDevice
+
+echo "edit /etc/sysconfig/network-scripts/ifcfg-$currentDevice and set ONBOOT=yes"
+sed -i "s/^\(ONBOOT=\).*$/\1yes/g" /etc/sysconfig/network-scripts/ifcfg-$currentDevice

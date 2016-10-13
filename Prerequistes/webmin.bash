eaa53cbf4a064f75c03b883527fcce4d99949595
rabahi <marc.rabahi@gmail.com>
2016-10-13 16:11:51 +0200

    Update webmin.bash

--- Prerequistes/webmin.bash
+++ Prerequistes/webmin.bash
@@ -9,12 +9,16 @@ echo "webprefixnoredir=1" >> /etc/webmin/config
 echo "referers=localhost" >> /etc/webmin/config
 echo "referer=1" >> /etc/webmin/config
 
+echo "configure httpd proxy"
 cat > /etc/httpd/conf.d/webmin.conf << "EOF"
 ProxyPreserveHost On
 Proxypass /webmin http://localhost:10000
 Proxypassreverse /webmin http://localhost:10000
 ProxyRequests Off
 EOF
+
+echo "remove ssl (for httpd proxy)"
+sed -i "s/^\(ssl=\).*/\10/" /etc/webmin/miniserv.conf
 
 echo "launch webmin"
 systemctl start webmin.service

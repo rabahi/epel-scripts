f2e62382f5223422270960738ba436f4ace49337
rabahi <marc.rabahi@gmail.com>
2016-10-14 13:48:10 +0200

    Update external-repos.bash

--- Prerequistes/external-repos.bash
+++ Prerequistes/external-repos.bash
@@ -3,8 +3,8 @@
 echo "get release rpm from fedora"
 yum -y install epel-release
 
-echo "enable Red Hat Software Collections (SCL) repository"
-yum -y install centos-release-scl
+#echo "enable Red Hat Software Collections (SCL) repository"
+#yum -y install centos-release-scl
 
 echo "add webmin repository"
 cat > /etc/yum.repos.d/webmin.repo << "EOF"

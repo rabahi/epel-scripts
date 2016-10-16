95ff91870500fac0e3c97bf7839d871be9f19582
rabahi <marc.rabahi@gmail.com>
2016-10-16 12:15:52 +0200

    Update mariadb.bash

--- Database/srv-database/mariadb.bash
+++ Database/srv-database/mariadb.bash
@@ -14,9 +14,7 @@ echo "set password for root. WARN! for this example we choose unsecure password 
 
 echo "Note: by default only local users can access to phpMyAdmin."
 echo "Let's update the file /etc/httpd/conf.d/phpMyAdmin.conf and allow everyone."
-sed -i "s/\(Deny\s*from\s*All\)/#\1/i" /etc/httpd/conf.d/phpMyAdmin.conf
-sed -i "s/\(Allow\s*from\s*127.0.0.1\)/#\1/i" /etc/httpd/conf.d/phpMyAdmin.conf
-sed -i "s/\(Allow\s*from\s*::1\)/#\1/i" /etc/httpd/conf.d/phpMyAdmin.conf
+sed -i "s/\(Require\s*ip\s*127.0.0.1\)/Require all granted\n\1/i" /etc/httpd/conf.d/phpMyAdmin.conf
 
 #Uncomment these following lines to open firewall
 #echo "add service to firewalld"

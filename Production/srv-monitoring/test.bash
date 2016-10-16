cd72249d4c7f2549da57f780e87d53f180c206cd
rabahi <none@none>
2016-10-16 11:13:21 +0200

    remove ocsinventory.

--- Production/srv-monitoring/test.bash
+++ Production/srv-monitoring/test.bash
@@ -6,7 +6,6 @@
 # check pages
 check_web_function nagios "http://localhost/nagios/ --http-user=nagiosadmin --http-password=nagiosadmin"
 check_web_function Centreon http://localhost/centreon/
-check_web_function OCS http://localhost/ocsreports/install.php
 check_web_function GLPI http://localhost/glpi/install/install.php
 check_web_function ntopng http://localhost/ntopng/
 

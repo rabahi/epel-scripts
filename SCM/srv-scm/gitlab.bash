98afb36fe7bd36ad87daf92c9bcf82943d188ce8
rabahi <marc.rabahi@gmail.com>
2017-04-12 16:05:09 +0200

    Update gitlab.bash

--- SCM/srv-scm/gitlab.bash
+++ SCM/srv-scm/gitlab.bash
@@ -6,7 +6,7 @@ dnf -y install gitlab-ce
 gitlab-ctl reconfigure
 
 echo "configure external url for gitlab"
-sed -i "s/^\(external_url\s*\).*/\1'http:\/\/localhost:8080\/gitlab'/" /etc/gitlab/gitlab.rb
+sed -i "s/^\(external_url\s*\).*/\1'http:\/\/localhost:6745\/gitlab'/" /etc/gitlab/gitlab.rb
 sed -i "s/^#\s*\(nginx\['enable'\]\s*=\s*\).*/\1false/" /etc/gitlab/gitlab.rb
 sed -i "s/^#\s*\(web_server\['external_users'\]\s*=\s*\).*/\1\['apache'\]/" /etc/gitlab/gitlab.rb
 gitlab-ctl reconfigure
@@ -15,8 +15,8 @@ gitlab-ctl restart
 echo "configure httpd (create /etc/httpd/conf.d/gitlab.conf)"
 cat > /etc/httpd/conf.d/gitlab.conf << "EOF"
 ProxyPreserveHost On
-Proxypass /gitlab http://localhost:8080/gitlab
-Proxypassreverse /gitlab http://localhost:8080/gitlab
+Proxypass /gitlab http://localhost:6745/gitlab
+Proxypassreverse /gitlab http://localhost:6745/gitlab
 ProxyRequests     Off
 EOF
 

646c17490c0ece2948f85dfcb3164339370d108d
rabahi <marc.rabahi@gmail.com>
2017-04-12 17:12:15 +0200

    Update gitlab.bash

--- SCM/srv-scm/gitlab.bash
+++ SCM/srv-scm/gitlab.bash
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+##################################################
+#      PARAMETERS 
+##################################################
+
+listen_port=6745
+
+##################################################
+#      INSTALLATION SCRIPT
+##################################################
 echo "install gitlab"
 curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
 dnf -y install gitlab-ce
 gitlab-ctl reconfigure
 
 echo "configure external url for gitlab"
-sed -i "s/^\(external_url\s*\).*/\1'http:\/\/localhost:6745\/gitlab'/" /etc/gitlab/gitlab.rb
-sed -i "s/^#\s*\(nginx\['enable'\]\s*=\s*\).*/\1false/" /etc/gitlab/gitlab.rb
-sed -i "s/^#\s*\(web_server\['external_users'\]\s*=\s*\).*/\1\['apache'\]/" /etc/gitlab/gitlab.rb
+sed -i "s/^\(external_url\s*\).*/\1'http:\/\/localhost:$listen_port\/gitlab'/" /etc/gitlab/gitlab.rb
 gitlab-ctl reconfigure
 gitlab-ctl restart
 
 echo "configure httpd (create /etc/httpd/conf.d/gitlab.conf)"
-cat > /etc/httpd/conf.d/gitlab.conf << "EOF"
+cat > /etc/httpd/conf.d/gitlab.conf << EOF
 ProxyPreserveHost On
-Proxypass /gitlab http://localhost:6745/gitlab
-Proxypassreverse /gitlab http://localhost:6745/gitlab
+Proxypass /gitlab http://localhost:$listen_port/gitlab
+Proxypassreverse /gitlab http://localhost:$listen_port/gitlab
 ProxyRequests     Off
 EOF
 

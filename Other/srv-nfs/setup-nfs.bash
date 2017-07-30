c99d7719d39bbac99bcc7735ebea355d8abc3940
rabahi <marc.rabahi@gmail.com>
2017-07-30 14:39:02 +0200

    Update setup-nfs.bash

--- Other/srv-nfs/setup-nfs.bash
+++ Other/srv-nfs/setup-nfs.bash
@@ -1,15 +1,13 @@
 echo "install nfs tools"
-dnf -y install nfs-utils nfs-utils-lib
+dnf -y install nfs-utils
  
 echo "start nfs on startup"
 systemctl enable rpcbind.service
 systemctl enable nfs.service
-#systemctl enable nfs-server.service
  
 echo "start service"
 systemctl start rpcbind.service
 systemctl start nfs.service
-#systemctl start nfs-server.service
  
 echo "add right to anonymous user NFS (id 65534)"
 mkdir -p /opt/nfs
@@ -27,7 +25,6 @@ exportfs -a
 echo "add service nfs (ports 111,2049,4045) to firewall"
 echo "to find port list : rpcinfo -p"
 firewall-cmd --permanent --add-service nfs
-#firewall-cmd --permanent --add-service rpc-bind
 
 echo "reload firewall-cmd"
 firewall-cmd --reload

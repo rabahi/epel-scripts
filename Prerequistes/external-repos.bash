a9037e83d8a573153c436657877b251b5eb037c7
rabahi <marc.rabahi@gmail.com>
2018-05-12 11:27:11 +0200

    Update external-repos.bash

--- Prerequistes/external-repos.bash
+++ Prerequistes/external-repos.bash
@@ -6,16 +6,6 @@ yum -y install epel-release
 #echo "enable Red Hat Software Collections (SCL) repository"
 #yum -y install centos-release-scl
 
-echo "add webmin repository"
-cat > /etc/yum.repos.d/webmin.repo << "EOF"
-[Webmin]
-name=Webmin Distribution Neutral
-#baseurl=http://download.webmin.com/download/yum
-mirrorlist=http://download.webmin.com/download/yum/mirrorlist
-enabled=1
-EOF
-rpm --import http://www.webmin.com/jcameron-key.asc
-
 echo "clean all and update"
 yum clean all
 yum -y install deltarpm

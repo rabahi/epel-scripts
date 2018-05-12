e6cc409289637141d889778d13e9bb549d9d8bf7
rabahi <marc.rabahi@gmail.com>
2018-05-12 11:33:10 +0200

    Update autoupdate.bash

--- Prerequistes/autoupdate.bash
+++ Prerequistes/autoupdate.bash
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-echo "install dnf-automatic"
-dnf -y install dnf-automatic
-
-echo "enable 'apply_updates'"
-sed -i "s/^\(apply_updates\s*=\s*\).*/\1yes/g" /etc/dnf/automatic.conf
-
-echo "activate dnf-automatic at startup"
-systemctl enable dnf-automatic.timer
-
-echo "start service"
-systemctl start dnf-automatic.timer
-
-## OLD Package Manager
+## Package Manager
 echo "install yum-cron"
-dnf -y install yum-cron
+yum -y install yum-cron
 
 echo "activate yum-cron at startup"
 systemctl enable yum-cron.service
